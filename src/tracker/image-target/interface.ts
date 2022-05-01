8270d70e79ede8d2d73f3d5320e0a5f3e58bf62e
KRSBX <firdaus.sati51@gmail.com>
2022-05-01 20:27:17 +0700

    feat: image target tracker

---
+++ src/tracker/image-target/interface.ts
@@ -0,0 +1,29 @@
+import { CumulativeSum } from '../../utils/image-target';
+
+interface IBaseOptions {
+  image: ImageData;
+  imageDataCumsum: CumulativeSum;
+  imageDataSqrCumsum: CumulativeSum;
+  sdThresh: number;
+}
+
+interface IOptions extends IBaseOptions {
+  featureMap: Float32Array;
+  templateSize: number;
+  searchSize: number;
+  occSize: number;
+  maxSimThresh: number;
+  minSimThresh: number;
+}
+
+interface ISimiliarityOptions extends Omit<IBaseOptions, 'sdThresh'> {
+  cx: number;
+  cy: number;
+  tx: number;
+  ty: number;
+  vlen: number;
+}
+
+interface ITemplateOptions extends IBaseOptions, Pick<ISimiliarityOptions, 'cx' | 'cy'> {}
+
+export type { IOptions, ISimiliarityOptions, ITemplateOptions };
