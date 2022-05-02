055e3d4d2076c246803614d27add8f896672915b
KRSBX <firdaus.sati51@gmail.com>
2022-05-02 19:45:55 +0700

    feat: image target detector

---
+++ src/detector/image-target/crop-detector.ts
@@ -0,0 +1,88 @@
+import tf from '@tensorflow/tfjs';
+import { Detector } from './detector';
+
+class CropDetector {
+  private width: number;
+  private height: number;
+  private debugMode: boolean;
+  private cropSize: number;
+  private detector: Detector;
+  private kernelCaches: Record<any, any>;
+  private lastRandomIndex: number;
+
+  constructor(width: number, height: number, debugMode = false) {
+    this.debugMode = debugMode;
+    this.width = width;
+    this.height = height;
+
+    // nearest power of 2, min dimensions
+    const minDimension = Math.min(width, height) / 2;
+    const cropSize = Math.pow(2, Math.round(Math.log(minDimension) / Math.log(2)));
+
+    this.cropSize = cropSize;
+
+    this.detector = new Detector(cropSize, cropSize, debugMode);
+
+    this.kernelCaches = {};
+    this.lastRandomIndex = 4;
+  }
+
+  detect(inputImageT: tf.Tensor<tf.Rank>) {
+    // crop center
+    const startY = Math.floor(this.height / 2 - this.cropSize / 2);
+    const startX = Math.floor(this.width / 2 - this.cropSize / 2);
+    const result = this._detect(inputImageT, startX, startY) as any;
+
+    if (this.debugMode && result.debugExtra)
+      result.debugExtra = {
+        ...result.debugExtra,
+        crop: {
+          startX,
+          startY,
+          cropSize: this.cropSize,
+        },
+      };
+
+    return result;
+  }
+
+  detectMoving(inputImageT: tf.Tensor<tf.Rank>) {
+    // loop a few locations around center
+    const dx = this.lastRandomIndex % 3;
+    const dy = Math.floor(this.lastRandomIndex / 3);
+
+    let startY = Math.floor(this.height / 2 - this.cropSize + (dy * this.cropSize) / 2);
+    let startX = Math.floor(this.width / 2 - this.cropSize + (dx * this.cropSize) / 2);
+
+    if (startX < 0) startX = 0;
+    if (startY < 0) startY = 0;
+    if (startX >= this.width - this.cropSize) startX = this.width - this.cropSize - 1;
+    if (startY >= this.height - this.cropSize) startY = this.height - this.cropSize - 1;
+
+    this.lastRandomIndex = (this.lastRandomIndex + 1) % 9;
+
+    const result = this._detect(inputImageT, startX, startY);
+
+    return result;
+  }
+
+  _detect(inputImageT: tf.Tensor<tf.Rank>, startX: number, startY: number) {
+    const cropInputImageT = inputImageT.slice([startY, startX], [this.cropSize, this.cropSize]);
+
+    const { featurePoints, ...result } = this.detector.detect(cropInputImageT);
+    const debugExtra = result.debugExtra as any;
+
+    featurePoints.forEach((p) => {
+      p.x += startX;
+      p.y += startY;
+    });
+
+    if (this.debugMode && debugExtra) debugExtra.projectedImage = cropInputImageT.arraySync();
+
+    cropInputImageT.dispose();
+
+    return { featurePoints: featurePoints, debugExtra };
+  }
+}
+
+export { CropDetector };
---
+++ src/input-loader/image-target.ts
@@ -0,0 +1,94 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import tf from '@tensorflow/tfjs';
+import { GPGPUProgram, MathBackendWebGL } from '@tensorflow/tfjs-backend-webgl';
+
+// More efficient implementation for tf.browser.fromPixels
+//   original implementation: /node_modules/@tensorflow/tfjs-backend-webgl/src/kernels/FromPixels.ts
+//
+// This implementation return grey scale instead of RGBA in the orignal implementation
+class InputLoader {
+  private width: number;
+  private height: number;
+  private textShape: number[];
+  private context: CanvasRenderingContext2D;
+  private tempPixelHandle: tf.TensorInfo;
+  private program: GPGPUProgram;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    this.textShape = [height, width];
+
+    const canvas = document.createElement('canvas') as unknown as HTMLCanvasElement;
+
+    const context = canvas.getContext('2d')!;
+    context.canvas.width = width;
+    context.canvas.height = height;
+    this.context = context;
+
+    this.program = this.buildProgram(width, height);
+
+    const backend = tf.backend() as MathBackendWebGL;
+
+    this.tempPixelHandle = backend.makeTensorInfo(this.textShape, 'float32');
+    // warning!!!
+    // usage type should be TextureUsage.PIXELS, but tfjs didn't export this enum type, so we hard-coded 2 here
+    //   i.e. backend.texData.get(tempPixelHandle.dataId).usage = TextureUsage.PIXELS;
+    backend.texData.get(this.tempPixelHandle.dataId).usage = 2;
+  }
+
+  // old method
+  _loadInput(input: ImageData) {
+    return tf.tidy(() => {
+      const inputImage = tf.browser.fromPixels(input).mean(2);
+
+      return inputImage;
+    });
+  }
+
+  // input is instance of HTMLVideoElement or HTMLImageElement
+  loadInput(input: CanvasImageSource) {
+    this.context.drawImage(input, 0, 0, this.width, this.height);
+
+    const backend = tf.backend() as MathBackendWebGL;
+
+    backend.gpgpu.uploadPixelDataToTexture(
+      backend.getTexture(this.tempPixelHandle.dataId),
+      this.context.canvas
+    );
+
+    const res = this._compileAndRun(this.program, [this.tempPixelHandle]);
+
+    return res;
+  }
+
+  buildProgram(width: number, height: number) {
+    const textureMethod = tf.env().getNumber('WEBGL_VERSION') === 2 ? 'texture' : 'texture2D';
+
+    const program = {
+      variableNames: ['A'],
+      outputShape: this.textShape,
+      userCode: `
+	void main() {
+	  ivec2 coords = getOutputCoords();
+	  int texR = coords[0];
+	  int texC = coords[1];
+	  vec2 uv = (vec2(texC, texR) + halfCR) / vec2(${width}.0, ${height}.0);
+
+	  vec4 values = ${textureMethod}(A, uv);
+	  setOutput((0.299 * values.r + 0.587 * values.g + 0.114 * values.b) * 255.0);
+	}
+      `,
+    };
+
+    return program;
+  }
+
+  _compileAndRun(program: GPGPUProgram, inputs: tf.TensorInfo[]) {
+    const outInfo = (tf.backend() as MathBackendWebGL).compileAndRun(program, inputs);
+
+    return tf.engine().makeTensorFromDataId(outInfo.dataId, outInfo.shape, outInfo.dtype);
+  }
+}
+
+export { InputLoader };
