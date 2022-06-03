5dbb58fc854dd06a28a84badc8cb0aad5d0a0a5c
KRSBX <firdaus.sati51@gmail.com>
2022-06-03 23:50:19 +0700

    feat: experimental image-target estimator

---
+++ src/experimental/image-target/utils/constant/estimation.ts
@@ -0,0 +1,6 @@
+export const TRACKING_THRESH = 5.0; // default
+export const K2_FACTOR = 4.0; // Question: should it be relative to the size of the screen instead of hardcoded?
+export const ICP_MAX_LOOP = 10;
+export const ICP_BREAK_LOOP_ERROR_THRESH = 0.1;
+export const ICP_BREAK_LOOP_ERROR_RATIO_THRESH = 0.99;
+export const ICP_BREAK_LOOP_ERROR_THRESH2 = 4.0;
