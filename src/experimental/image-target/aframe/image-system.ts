4dde4f0f221e03b636212a5797fed472a2b11486
KRSBX <firdaus.sati51@gmail.com>
2022-06-05 00:49:20 +0700

    fix: experimental image-target wont hide ui

--- src/experimental/image-target/aframe/image-system.ts
+++ src/experimental/image-target/aframe/image-system.ts
@@ -83,7 +83,9 @@ AFRAME.registerSystem(AR_COMPONENT_NAME.IMAGE_SYSTEM, {
   _registerEventListener: function () {
     // Subcribe to the targetFound event
     // This event is fired when the target is found
-    this.el.addEventListener(AR_EVENT_NAME.MARKER_FOUND, this.ui.hideScanning.bind(this));
+    this.el.addEventListener(AR_EVENT_NAME.MARKER_FOUND, () => {
+      this.ui.hideScanning();
+    });
 
     // Subcribe to the targetFound event
     // This event is fired when the target is found
