59ef4b65b953aa66b059422639d7f1b8380a48ea
KRSBX <firdaus.sati51@gmail.com>
2022-06-07 08:41:15 +0700

    feat: face-target examples

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,9 @@
 import './style.css';
+import { EXAMPLE } from './utils/constant';
 
 const app = document.querySelector('#app') as unknown as HTMLDivElement;
 
-const SCENE = `
-  <a-scene mindar-image="imageTargetSrc: https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/card.mind;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
-      <a-assets>
-        <img id="card" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/card.png" />
-        <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
-      </a-assets>
-
-      <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-      <a-entity mindar-image-target="targetIndex: 0">
-        <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
-        <a-gltf-model rotation="0 0 0 " position="0 0 0.1" scale="0.005 0.005 0.005" src="#avatarModel" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate">
-      </a-entity>
-    </a-scene>
-`;
+const SCENE = EXAMPLE.FACE_TARGET;
 
 app.innerHTML = `
   <h1>Hello Mind AR J/TS!</h1>
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,11 +1,11 @@
-const STATS_STYLE = 'position: absolute; top: 0px; left: 0px; z-index: 999';
+export const STATS_STYLE = 'position: absolute; top: 0px; left: 0px; z-index: 999';
 
-const GLOBAL_AR_EVENT_NAME = {
+export const GLOBAL_AR_EVENT_NAME = {
   SCREEN_RESIZE: 'resize',
   LOADED_METADATA: 'loadedmetadata',
 };
 
-const AR_ELEMENT_TAG = {
+export const AR_ELEMENT_TAG = {
   A_CAMERA: 'a-camera',
   A_ENTITY: 'a-entity',
   A_SCENE: 'a-scene',
@@ -13,30 +13,51 @@ const AR_ELEMENT_TAG = {
   MESH: 'mesh',
 };
 
-const AR_STATE = {
+export const AR_STATE = {
   RENDER_START: 'renderstart',
   AR_ERROR: 'arError',
   AR_READY: 'arReady',
 };
 
-const CONFIRMATION = {
+export const CONFIRMATION = {
   YES: 'yes',
   NO: 'no',
 };
 
-const HIDDEN_CLASS_NAME = 'hidden';
+export const HIDDEN_CLASS_NAME = 'hidden';
 
-const PRODUCTION = 'production';
+export const PRODUCTION = 'production';
 
-const IS_PRODUCTION =
+export const IS_PRODUCTION =
   import.meta.env?.VITE_ENV === PRODUCTION ?? process.env?.NODE_ENV === PRODUCTION;
 
-export {
-  STATS_STYLE,
-  GLOBAL_AR_EVENT_NAME,
-  AR_ELEMENT_TAG,
-  AR_STATE,
-  CONFIRMATION,
-  HIDDEN_CLASS_NAME,
-  IS_PRODUCTION,
+export const EXAMPLE = {
+  FACE_TARGET: `
+  <a-scene mindar-face embedded color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+    <a-assets>
+      <a-asset-item id="headModel" src="assets/sparkar/headOccluder.glb"></a-asset-item>
+      <a-asset-item id="glassesModel" src="assets/glasses/scene.gltf"></a-asset-item>
+    </a-assets>
+    <a-camera active="false" position="0 0 0" look-controls="enabled: false"></a-camera>
+    <a-entity mindar-face-target="anchorIndex: 168">
+      <a-gltf-model mindar-face-occluder position="0 -0.3 0.15"rotation="0 0 0" scale="0.065 0.065 0.065" src="#headModel"></a-gltf-model>
+    </a-entity>
+    <a-entity mindar-face-target="anchorIndex: 168">
+      <a-gltf-model rotation="0 0 0" position="0 0.15 0" scale="0.008 0.01 0.01" src="#glassesModel"></a-gltf-model>
+    </a-entity>
+  </a-scene>
+  `,
+  IMAGE_TARGET: `
+  <a-scene mindar-image="imageTargetSrc: https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/card.mind;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+    <a-assets>
+      <img id="card" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/card.png" />
+      <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.4/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
+    </a-assets>
+    <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+    <a-entity mindar-image-target="targetIndex: 0">
+      <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
+      <a-gltf-model rotation="0 0 0 " position="0 0 0.1" scale="0.005 0.005 0.005" src="#avatarModel" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate">
+    </a-entity>
+  </a-scene>
+  `,
 };
